d495ed3db7c448aa6ba78405f29b56611807c7bb
Shaina <shainaz@udel.edu>
2025-04-09 16:59:56 -0400

    working on customer despawn

--- assets/CustomerManager.cs
+++ assets/CustomerManager.cs
@@ -6,20 +6,26 @@ public class CustomerManager : MonoBehaviour
     public GameObject Customer;
     public int maxCustomer=1;
     public int currentCustomer=0;
-
+    public int despawnTime=5;
+    public float customerTimer=0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       if(currentCustomer<maxCustomer){
-            Instantiate(Customer,transform.position,transform.rotation);
-            currentCustomer++;
-        } 
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        
+       if(currentCustomer<maxCustomer){
+            Instantiate(Customer,transform.position,transform.rotation);
+            currentCustomer++;
+        } 
+        customerTimer += Time.deltaTime;
+        if(customerTimer>despawnTime){
+            customerTimer=0;
+            Destroy(gameObject);
+            currentCustomer--;
+        }
     }
 }
