cbe179bb2fea81f1163332e6573c8f831c887ad1
Shaina <shainaz@udel.edu>
2025-04-09 15:32:20 -0400

    fixed customer spawning

--- assets/CustomerManager.cs
+++ assets/CustomerManager.cs
@@ -4,34 +4,22 @@ using System.Collections;
 public class CustomerManager : MonoBehaviour
 {
     public GameObject Customer;
-    public float spawnDelay = 5f;      
-
-    private bool customerSpawned = false;  
+    public int maxCustomer=1;
+    public int currentCustomer=0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      
+       if(currentCustomer<maxCustomer){
+            Instantiate(Customer,transform.position,transform.rotation);
+            currentCustomer++;
+        } 
     }
 
     // Update is called once per frame
     void Update()
     {
-         
-        if (!customerSpawned)
-        {
-            
-            StartCoroutine(SpawnCustomer());
-        }
-    }
-    private IEnumerator SpawnCustomer()
-    {
-        yield return new WaitForSeconds(spawnDelay);
-
+       
         
-        Instantiate(Customer, transform.position, Quaternion.identity);
-
-        
-        customerSpawned = true;
     }
 }
