75bab4229157d90ccc343fcbd2bb26d1e17a10d6
Shaina <shainaz@udel.edu>
2025-04-16 14:15:00 -0400

    added new global manager script

---
+++ assets/GlobalManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GlobalManager : MonoBehaviour
+{
+     public GameObject Customer;
+    public int maxCustomer=1;
+    public int currentCustomer=0;
+    public int despawnTime=30;
+    public float customerTimer=0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(currentCustomer<maxCustomer){
+            Instantiate(Customer,transform.position,transform.rotation);
+            currentCustomer++;
+        } 
+        customerTimer += Time.deltaTime;
+        if(customerTimer>despawnTime){
+            customerTimer=0;
+            Destroy(Customer);
+            currentCustomer--;
+        }
+    }
+}
