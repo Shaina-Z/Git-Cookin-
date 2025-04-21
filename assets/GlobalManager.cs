1d380ce38a084e0a0df56f245c947ae68b06a9b0
Shaina <shainaz@udel.edu>
2025-04-21 14:44:10 -0400

    made time consistent across

--- assets/GlobalManager.cs
+++ assets/GlobalManager.cs
@@ -6,23 +6,23 @@ public class GlobalManager : MonoBehaviour
     public int maxCustomer=1;
     public int currentCustomer=0;
     public int despawnTime=30;
-    public float customerTimer=0;
+    public TimeManager TimeManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        TimeManager = GameObject.FindGameObjectWithTag("TimeManager").GetComponent<TimeManager>();
        
     }
 
     // Update is called once per frame
     void Update()
     {
-        customerTimer += Time.deltaTime;
          if(currentCustomer<maxCustomer){
            Instantiate(Customer,transform.position,transform.rotation);
             currentCustomer++;
         }
-        if(customerTimer>despawnTime){
-            customerTimer=0;
+        if(TimeManager.GameTime>despawnTime){
+            TimeManager.GameTime=0;
             var newCustomer = GameObject.Find("Customer(Clone)");
             Destroy(newCustomer);
             currentCustomer--;
--- assets/TimeManager.cs
+++ assets/TimeManager.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
-    public static float GameTime;
+    public  float GameTime;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
+       
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   DontDestroyOnLoad(gameObject);
         GameTime+= Time.deltaTime;
     }
 }
