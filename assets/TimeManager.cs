4ab41a0785054351471d9db369f871fc4b47e774
Shaina <shainaz@udel.edu>
2025-04-21 14:22:40 -0400

    made sure object doesnt reset

--- assets/TimeManager.cs
+++ assets/TimeManager.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
+    public static float GameTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameTime+= Time.deltaTime;
     }
 }
