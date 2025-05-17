6453abde04446933167368dc6a16f7eec6f15419
Shaina <shainaz@udel.edu>
2025-05-17 14:11:22 -0400

    added new text

--- assets/Scripts/TextManager.cs
+++ assets/Scripts/TextManager.cs
@@ -5,7 +5,8 @@ public class TextManager : MonoBehaviour
 {
     public Text customerText;
     public TimeManager TimeManager; 
-    public string[] customerSayings ={"One burger please!","One salad please!"};
+    public string[] customerSayings ={"One plain burger please!","One burger with lettuce please!",
+    "One burger with lettuce and tomato please!", "Give me the whole package!"};
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,7 +17,7 @@ public class TextManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        customerText.text=customerSayings[Random.Range(0,1)];
+        customerText.text=customerSayings[Random.Range(0,3)];
         if(TimeManager.GameTime==30){
             customerText.text="This sucks! I'm outta here!";
             WaitOneSecond();
