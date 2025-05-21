d39c941f64dfb5e37249d61e03fedf2ccecbeaf9
Axel Rodriguez-Leon <axelarl@udel.edu>
2025-05-21 06:13:50 -0400

    Fixing InputController

--- assets/Scripts/InputController.cs
+++ assets/Scripts/InputController.cs
@@ -5,19 +5,20 @@ using System.Collections.Generic;
 using System.Collections;
 public class InputController : MonoBehaviour
 {
-     public InputField inputField;
+    public InputField inputField;
     public GameObject Bun;
     public GameObject Patty;
     public GameObject Lettuce;
     public GameObject Tomato;
     public GameObject PlainBurger;
     public GameObject Placeholder;
-    public GlobalManager GlobalManager; 
-     public List<GameObject> Inventory;
+    public GlobalManager GlobalManager;
+    public List<GameObject> Inventory;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start(){   
-         inputField.onValueChanged.AddListener(OnInputFieldValueChanged);
-         GlobalManager = GameObject.FindGameObjectWithTag("GlobalManager").GetComponent<GlobalManager>();
+    void Start()
+    {
+        inputField.onValueChanged.AddListener(OnInputFieldValueChanged);
+        GlobalManager = GameObject.FindGameObjectWithTag("GlobalManager").GetComponent<GlobalManager>();
     }
 
     // Update is called once per frame
@@ -25,66 +26,73 @@ public class InputController : MonoBehaviour
     {
         string inputText = inputField.text;
     }
-     void OnInputFieldValueChanged(string inputText)
+    void OnInputFieldValueChanged(string inputText)
+    {
+        if (inputText == "git checkout kitchen")
         {
-            if(inputText=="git checkout kitchen"){
             SceneManager.LoadScene("Kitchen");
-        }else if(inputText=="git checkout main"){
+        }
+        else if (inputText == "git checkout main")
+        {
             SceneManager.LoadScene("Main");
         }
-        if(inputText=="git pull bun"){
-            Instantiate(Bun,Placeholder.transform);
+        if (inputText == "git pull bun")
+        {
+            Instantiate(Bun, Placeholder.transform);
             Inventory.Add(Bun);
         }
-        if (inputText=="git merge patty"){
+        if (inputText == "git merge patty")
+        {
             Instantiate(PlainBurger);
             DontDestroyOnLoad(GameObject.Find("PlainBurger(Clone)"));
         }
-        if(inputText=="git commit"&&GameObject.Find("PlainBurger(Clone)")){
-                GlobalManager.addPoint();
-        }
-        }
-
-
-public class InputController : MonoBehaviour
-{
-    public InputField inputField;
-    private string currentInputText = "";
-
-    void Start()
-    {
-        inputField.onValueChanged.AddListener(OnInputFieldValueChanged);
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        if (inputText == "git commit" && GameObject.Find("PlainBurger(Clone)"))
         {
-            if (currentInputText.Contains("git checkout kitchen"))
-            {
-                SceneManager.LoadScene("Kitchen");
-                Debug.Log("Skibidi Rizz");
-            }
-            else if (currentInputText.Contains("git checkout main"))
-            {
-                SceneManager.LoadScene("Main");
-                Debug.Log("Tralalero Tralala");
-            }
-            else if (currentInputText.Contains("git checkout fridge"))
-            {
-                SceneManager.LoadScene("Fridge");
-                Debug.Log("Ballerina Cappucina");
-            }
-            else if (currentInputText.Contains("git checkout pantry"))
-            {
-                SceneManager.LoadScene("Pantry");
-                Debug.Log("Brr Brr Patapim");
-            }
+            GlobalManager.addPoint();
         }
     }
 
-    void OnInputFieldValueChanged(string inputText)
+
+    public class InputController : MonoBehaviour
     {
-        currentInputText = inputText;
+        public InputField inputField;
+        private string currentInputText = "";
+
+        void Start()
+        {
+            inputField.onValueChanged.AddListener(OnInputFieldValueChanged);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                if (currentInputText.Contains("git checkout kitchen"))
+                {
+                    SceneManager.LoadScene("Kitchen");
+                    Debug.Log("Skibidi Rizz");
+                }
+                else if (currentInputText.Contains("git checkout main"))
+                {
+                    SceneManager.LoadScene("Main");
+                    Debug.Log("Tralalero Tralala");
+                }
+                else if (currentInputText.Contains("git checkout fridge"))
+                {
+                    SceneManager.LoadScene("Fridge");
+                    Debug.Log("Ballerina Cappucina");
+                }
+                else if (currentInputText.Contains("git checkout pantry"))
+                {
+                    SceneManager.LoadScene("Pantry");
+                    Debug.Log("Brr Brr Patapim");
+                }
+            }
+        }
+
+        void OnInputFieldValueChanged(string inputText)
+        {
+            currentInputText = inputText;
+        }
     }
 }
