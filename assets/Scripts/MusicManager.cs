296f85500fe32a094fb07956bbbeb0443d16d832
Shaina <shainaz@udel.edu>
2025-05-21 10:04:38 -0400

    music does not duplicate in scenes

--- assets/Scripts/MusicManager.cs
+++ assets/Scripts/MusicManager.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 public class MusicManager : MonoBehaviour
 {
     AudioSource Background_music;
+    public static MusicManager Instance;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Background_music = GetComponent<AudioSource>();
-        DontDestroyOnLoad(this.gameObject);
+        
     }
+void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // Destroy duplicate instances
+            return;
+        }
 
+        Instance = this;
+        DontDestroyOnLoad(gameObject); // This object will persist across scenes
+    }
     // Update is called once per frame
     void Update()
     {
